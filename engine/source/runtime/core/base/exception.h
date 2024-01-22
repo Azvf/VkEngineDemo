76fbd8bbfab2dd6d0fb34eb378b8a1250ca93db7
Azvf <mazhenyu284@gmail.com>
2024-01-23 01:00:52 +0800

    new: for now i have no intention to create my own kind of error code that maps all the underlying error codes that in the vendor libraries i use, so i'll just impelement a template exception class that throws each individual's error code

---
+++ engine/source/runtime/core/base/exception.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <string>
+#include <exception>
+#include <iostream>
+#include <sstream>
+#include <cassert>
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.h>
+
+namespace Chandelier {
+    template<typename Result>
+    class EngineException : public std::exception {
+    public:
+        EngineException(const std::string& err, const Result result)
+            : m_err_string(err), m_result(result) {}
+
+        virtual ~EngineException() throw() {}
+        virtual const char* what() const throw() { return m_err_string.c_str(); }
+        Result  GetErrorCode() const { return m_result; }
+        const std::string& GetErrorString() const { return m_err_string; }
+    
+    public:
+        static EngineException MakeEngineException(const std::string& err, const Result result,
+            const std::string& func_name, const std::string& file_name, int line_no) {
+            std::ostringstream err_log;
+            err_log << func_name << " : " << err << " at " << file_name << ":" << line_no << std::endl;
+            EngineException exception(err_log.str(), result);
+            return exception;
+        }
+    
+    private:
+        std::string m_err_string;
+        Result      m_result;
+    };
+}
+
+enum EngineCode {
+    Base_Engine_Code            = 2568,
+    Buffer_Size_Not_Match,
+    Image_Layout_Not_Supported,
+    None_Suitable_Mem_Type,
+    Engine_Code_MAX_ENUM,
+};
+
+#define ENGINE_THROW_ERROR( err_str, result )                           \
+    do {                                                                \
+        assert(0);                                                      \
+        throw EngineException<decltype(result)>::MakeEngineException(   \
+            err_str, result, __FUNCTION__, __FILE__, __LINE__           \
+        );                                                              \
+    } while (0)
+
+
+#define VULKAN_API_CALL( vulkan_api )                                       \
+    do {                                                                    \
+        VkResult result = vulkan_api;                                       \
+        if( result != VK_SUCCESS ) {                                        \
+            assert(0);                                                      \
+            std::ostringstream err_log;                                     \
+            err_log << #vulkan_api << " returned error " << result;         \
+            throw EngineException<VkResult>::MakeEngineException(           \
+                err_log.str(), result, __FUNCTION__, __FILE__, __LINE__     \
+            );                                                              \
+        }                                                                   \
+    } while (0)
+
