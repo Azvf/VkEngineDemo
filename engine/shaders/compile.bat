82799c7e51525bcd2270ef4e94ce26c719c97c05
Azvf <mazhenyu284@gmail.com>
2024-02-25 01:20:32 +0800

    update: append skybox draw pass glsl code

--- engine/shaders/compile.bat
+++ engine/shaders/compile.bat
@@ -1,3 +1,7 @@
-glslc.exe shader.vert -o vert.spv
-glslc.exe shader.frag -o frag.spv
+glslc.exe shader.vert -o generated/base_vert.spv
+glslc.exe shader.frag -o generated/base_frag.spv
+
+glslc.exe skybox.vert -o generated/skybox_vert.spv
+glslc.exe skybox.frag -o generated/skybox_frag.spv
+
 pause
